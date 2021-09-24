4807ffaf606918a37ec2b2a710a669a0be7e8b1b
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2021-09-24 16:26:07 +0300

    Update and rename goodbye.ps1 to UpdateModelVersion.ps1

---
+++ UpdateModelVersion.ps1
@@ -0,0 +1,340 @@
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [string]$xppSourcePath,
+        [Parameter()]
+        [string]$xppDescriptorSearch,
+        $FindOptions,
+        $xppLayer,
+        $versionNumber
+        )
+
+
+#$xppSourcePath = "C:\_bld\PackagesLocalDirectory"
+#$xppDescriptorSearch = "**\Descriptor\*.xml"
+#$xppLayer = "VAR"
+#$versionNumber = "2021.09.22.2"
+
+if ($xppDescriptorSearch.Contains("`n"))
+{
+    [string[]]$xppDescriptorSearch = $xppDescriptorSearch -split "`n"
+}
+
+Test-Path -LiteralPath $xppSourcePath -PathType Container
+    
+if ($versionNumber -match "^\d+\.\d+\.\d+\.\d+$")
+{
+    $versions = $versionNumber.Split('.')
+}
+else
+{
+    throw "Version Number '$versionNumber' is not of format #.#.#.#"
+}
+
+# Discover packages
+$BuildModuleDirectories = @(Get-ChildItem -Path $BuildMetadataDir -Directory)
+foreach ($BuildModuleDirectory in $BuildModuleDirectories)
+{
+    $potentialDescriptors = Find-Match -DefaultRoot $xppSourcePath -Pattern $xppDescriptorSearch | Where-Object { (Test-Path -LiteralPath $_ -PathType Leaf) }
+    if ($potentialDescriptors.Length -gt 0)
+    {
+        Write-Host "Found $($potentialDescriptors.Length) potential descriptors"
+
+        foreach ($descriptorFile in $potentialDescriptors)
+        {
+            try
+            {
+                [xml]$xml = Get-Content $descriptorFile -Encoding UTF8
+
+                $modelInfo = $xml.SelectNodes("/AxModelInfo")
+                if ($modelInfo.Count -eq 1)
+                {
+                    $layer = $xml.SelectNodes("/AxModelInfo/Layer")[0]
+                    $layerid = $layer.InnerText
+                    $layerid = [int]$layerid
+
+                    $modelName = ($xml.SelectNodes("/AxModelInfo/Name")).InnerText
+                        
+                    # If this model's layer is equal or above lowest layer specified
+                    if ($layerid -ge $xppLayer)
+                    {
+                        $version = $xml.SelectNodes("/AxModelInfo/VersionMajor")[0]
+                        $version.InnerText = $versions[0]
+
+                        $version = $xml.SelectNodes("/AxModelInfo/VersionMinor")[0]
+                        $version.InnerText = $versions[1]
+
+                        $version = $xml.SelectNodes("/AxModelInfo/VersionBuild")[0]
+                        $version.InnerText = $versions[2]
+
+                        $version = $xml.SelectNodes("/AxModelInfo/VersionRevision")[0]
+                        $version.InnerText = $versions[3]
+
+                        $xml.Save($descriptorFile)
+
+                        Write-Host " - Updated model $modelName version to $versionNumber in $descriptorFile"
+                    }
+                    else
+                    {
+                        Write-Host " - Skipped $modelName because it is in a lower layer in $descriptorFile"
+                    }
+                }
+                else
+                {
+                    Write-Host "##vso[task.logissue type=error] - File '$descriptorFile' is not a valid descriptor file"
+                }
+            }
+            catch
+            {
+                Write-Host "##vso[task.logissue type=error] - File '$descriptorFile' is not a valid descriptor file (exception: $($_.Exception.Message))"
+            }
+        }
+    }
+}
+
+
+
+
+
+
+function Find-Match {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [string]$DefaultRoot,
+        [Parameter()]
+        [string[]]$Pattern,
+        $FindOptions,
+        $MatchOptions)
+
+Install-Package Minimatch
+    $originalErrorActionPreference = $ErrorActionPreference
+    try {
+        $ErrorActionPreference = 'Stop'
+
+
+
+        Write-Verbose "DefaultRoot: '$DefaultRoot'"
+        if (!$FindOptions) {
+            $FindOptions = New-FindOptions -FollowSpecifiedSymbolicLink -FollowSymbolicLinks
+        }
+
+
+        if (!$MatchOptions) {
+            $MatchOptions = New-MatchOptions -Dot -NoBrace -NoCase
+        }
+
+
+        Add-Type -LiteralPath $PSScriptRoot\Minimatch.dll
+
+        # Normalize slashes for root dir.
+        $DefaultRoot = ConvertTo-NormalizedSeparators -Path $DefaultRoot
+
+        $results = @{ }
+        $originalMatchOptions = $MatchOptions
+        foreach ($pat in $Pattern) {
+            Write-Verbose "Pattern: '$pat'"
+
+            # Trim and skip empty.
+            $pat = "$pat".Trim()
+            if (!$pat) {
+                Write-Verbose 'Skipping empty pattern.'
+                continue
+            }
+
+            # Clone match options.
+            $MatchOptions = Copy-MatchOptions -Options $originalMatchOptions
+
+            # Skip comments.
+            if (!$MatchOptions.NoComment -and $pat.StartsWith('#')) {
+                Write-Verbose 'Skipping comment.'
+                continue
+            }
+
+            # Set NoComment. Brace expansion could result in a leading '#'.
+            $MatchOptions.NoComment = $true
+
+            # Determine whether pattern is include or exclude.
+            $negateCount = 0
+            if (!$MatchOptions.NoNegate) {
+                while ($negateCount -lt $pat.Length -and $pat[$negateCount] -eq '!') {
+                    $negateCount++
+                }
+
+                $pat = $pat.Substring($negateCount) # trim leading '!'
+                if ($negateCount) {
+                    Write-Verbose "Trimmed leading '!'. Pattern: '$pat'"
+                }
+            }
+
+            $isIncludePattern = $negateCount -eq 0 -or
+                ($negateCount % 2 -eq 0 -and !$MatchOptions.FlipNegate) -or
+                ($negateCount % 2 -eq 1 -and $MatchOptions.FlipNegate)
+
+            # Set NoNegate. Brace expansion could result in a leading '!'.
+            $MatchOptions.NoNegate = $true
+            $MatchOptions.FlipNegate = $false
+
+            # Trim and skip empty.
+            $pat = "$pat".Trim()
+            if (!$pat) {
+                Write-Verbose 'Skipping empty pattern.'
+                continue
+            }
+
+            # Expand braces - required to accurately interpret findPath.
+            $expanded = $null
+            $preExpanded = $pat
+            if ($MatchOptions.NoBrace) {
+                $expanded = @( $pat )
+            } else {
+                # Convert slashes on Windows before calling braceExpand(). Unfortunately this means braces cannot
+                # be escaped on Windows, this limitation is consistent with current limitations of minimatch (3.0.3).
+                Write-Verbose "Expanding braces."
+                $convertedPattern = $pat -replace '\\', '/'
+                $expanded = [Minimatch.Minimatcher]::BraceExpand(
+                    $convertedPattern,
+                    (ConvertTo-MinimatchOptions -Options $MatchOptions))
+            }
+
+            # Set NoBrace.
+            $MatchOptions.NoBrace = $true
+
+            foreach ($pat in $expanded) {
+                if ($pat -ne $preExpanded) {
+                    Write-Verbose "Pattern: '$pat'"
+                }
+
+                # Trim and skip empty.
+                $pat = "$pat".Trim()
+                if (!$pat) {
+                    Write-Verbose "Skipping empty pattern."
+                    continue
+                }
+
+                if ($isIncludePattern) {
+                    # Determine the findPath.
+                    $findInfo = Get-FindInfoFromPattern -DefaultRoot $DefaultRoot -Pattern $pat -MatchOptions $MatchOptions
+                    $findPath = $findInfo.FindPath
+                    Write-Verbose "FindPath: '$findPath'"
+
+                    if (!$findPath) {
+                        Write-Verbose "Skipping empty path."
+                        continue
+                    }
+
+                    # Perform the find.
+                    Write-Verbose "StatOnly: '$($findInfo.StatOnly)'"
+                    [string[]]$findResults = @( )
+                    if ($findInfo.StatOnly) {
+                        # Simply stat the path - all path segments were used to build the path.
+                        if ((Test-Path -LiteralPath $findPath)) {
+                            $findResults += $findPath
+                        }
+                    } else {
+                        $findResults = Get-FindResult -Path $findPath -Options $FindOptions
+                    }
+
+                    Write-Verbose "Found $($findResults.Count) paths."
+
+                    # Apply the pattern.
+                    Write-Verbose "Applying include pattern."
+                    if ($findInfo.AdjustedPattern -ne $pat) {
+                        Write-Verbose "AdjustedPattern: '$($findInfo.AdjustedPattern)'"
+                        $pat = $findInfo.AdjustedPattern
+                    }
+
+                    $matchResults = [Minimatch.Minimatcher]::Filter(
+                        $findResults,
+                        $pat,
+                        (ConvertTo-MinimatchOptions -Options $MatchOptions))
+
+                    # Union the results.
+                    $matchCount = 0
+                    foreach ($matchResult in $matchResults) {
+                        $matchCount++
+                        $results[$matchResult.ToUpperInvariant()] = $matchResult
+                    }
+
+                    Write-Verbose "$matchCount matches"
+                } else {
+                    # Check if basename only and MatchBase=true.
+                    if ($MatchOptions.MatchBase -and
+                        !(Test-Rooted -Path $pat) -and
+                        ($pat -replace '\\', '/').IndexOf('/') -lt 0) {
+
+                        # Do not root the pattern.
+                        Write-Verbose "MatchBase and basename only."
+                    } else {
+                        # Root the exclude pattern.
+                        $pat = Get-RootedPattern -DefaultRoot $DefaultRoot -Pattern $pat
+                        Write-Verbose "After Get-RootedPattern, pattern: '$pat'"
+                    }
+
+                    # Apply the pattern.
+                    Write-Verbose 'Applying exclude pattern.'
+                    $matchResults = [Minimatch.Minimatcher]::Filter(
+                        [string[]]$results.Values,
+                        $pat,
+                        (ConvertTo-MinimatchOptions -Options $MatchOptions))
+
+                    # Subtract the results.
+                    $matchCount = 0
+                    foreach ($matchResult in $matchResults) {
+                        $matchCount++
+                        $results.Remove($matchResult.ToUpperInvariant())
+                    }
+
+                    Write-Verbose "$matchCount matches"
+                }
+            }
+        }
+
+        $finalResult = @( $results.Values | Sort-Object )
+        Write-Verbose "$($finalResult.Count) final results"
+        return $finalResult
+    } catch {
+        $ErrorActionPreference = $originalErrorActionPreference
+        Write-Error $_
+    } 
+}
+
+function New-FindOptions {
+    [CmdletBinding()]
+    param(
+        [switch]$FollowSpecifiedSymbolicLink,
+        [switch]$FollowSymbolicLinks)
+
+    return New-Object psobject -Property @{
+        FollowSpecifiedSymbolicLink = $FollowSpecifiedSymbolicLink.IsPresent
+        FollowSymbolicLinks = $FollowSymbolicLinks.IsPresent
+    }
+}
+
+function New-MatchOptions {
+    [CmdletBinding()]
+    param(
+        [switch]$Dot,
+        [switch]$FlipNegate,
+        [switch]$MatchBase,
+        [switch]$NoBrace,
+        [switch]$NoCase,
+        [switch]$NoComment,
+        [switch]$NoExt,
+        [switch]$NoGlobStar,
+        [switch]$NoNegate,
+        [switch]$NoNull)
+
+    return New-Object psobject -Property @{
+        Dot = $Dot.IsPresent
+        FlipNegate = $FlipNegate.IsPresent
+        MatchBase = $MatchBase.IsPresent
+        NoBrace = $NoBrace.IsPresent
+        NoCase = $NoCase.IsPresent
+        NoComment = $NoComment.IsPresent
+        NoExt = $NoExt.IsPresent
+        NoGlobStar = $NoGlobStar.IsPresent
+        NoNegate = $NoNegate.IsPresent
+        NoNull = $NoNull.IsPresent
+    }
+}
