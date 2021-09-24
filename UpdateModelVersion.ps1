6e80edba7b314cad23ab54b7553c52d298a5e34e
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2021-09-24 17:15:52 +0300

    Update UpdateModelVersion.ps1

--- UpdateModelVersion.ps1
+++ UpdateModelVersion.ps1
@@ -250,10 +250,231 @@ function New-MatchOptions {
         NoNull = $NoNull.IsPresent
     }
 }
-#$xppSourcePath = "C:\_bld\PackagesLocalDirectory"
-#$xppDescriptorSearch = "**\Descriptor\*.xml"
-#$xppLayer = "VAR"
-#$versionNumber = "2021.09.22.2"
+
+function ConvertTo-NormalizedSeparators {
+    [CmdletBinding()]
+    param([string]$Path)
+
+    # Convert slashes.
+    $Path = "$Path".Replace('/', '\')
+
+    # Remove redundant slashes.
+    $isUnc = $Path -match '^\\\\+[^\\]'
+    $Path = $Path -replace '\\\\+', '\'
+    if ($isUnc) {
+        $Path = '\' + $Path
+    }
+
+    return $Path
+}
+
+function Get-FindInfoFromPattern {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$DefaultRoot,
+        [Parameter(Mandatory = $true)]
+        [string]$Pattern,
+        [Parameter(Mandatory = $true)]
+        $MatchOptions)
+
+    if (!$MatchOptions.NoBrace) {
+        throw "Get-FindInfoFromPattern expected MatchOptions.NoBrace to be true."
+    }
+
+    # For the sake of determining the find path, pretend NoCase=false.
+    $MatchOptions = Copy-MatchOptions -Options $MatchOptions
+    $MatchOptions.NoCase = $false
+
+    # Check if basename only and MatchBase=true
+    if ($MatchOptions.MatchBase -and
+        !(Test-Rooted -Path $Pattern) -and
+        ($Pattern -replace '\\', '/').IndexOf('/') -lt 0) {
+
+        return New-Object psobject -Property @{
+            AdjustedPattern = $Pattern
+            FindPath = $DefaultRoot
+            StatOnly = $false
+        }
+    }
+
+    # The technique applied by this function is to use the information on the Minimatch object determine
+    # the findPath. Minimatch breaks the pattern into path segments, and exposes information about which
+    # segments are literal vs patterns.
+    #
+    # Note, the technique currently imposes a limitation for drive-relative paths with a glob in the
+    # first segment, e.g. C:hello*/world. It's feasible to overcome this limitation, but is left unsolved
+    # for now.
+    $minimatchObj = New-Object Minimatch.Minimatcher($Pattern, (ConvertTo-MinimatchOptions -Options $MatchOptions))
+
+    # The "set" field is a two-dimensional enumerable of parsed path segment info. The outer enumerable should only
+    # contain one item, otherwise something went wrong. Brace expansion can result in multiple items in the outer
+    # enumerable, but that should be turned off by the time this function is reached.
+    #
+    # Note, "set" is a private field in the .NET implementation but is documented as a feature in the nodejs
+    # implementation. The .NET implementation is a port and is by a different author.
+    $setFieldInfo = $minimatchObj.GetType().GetField('set', 'Instance,NonPublic')
+    [object[]]$set = $setFieldInfo.GetValue($minimatchObj)
+    if ($set.Count -ne 1) {
+        throw "Get-FindInfoFromPattern expected Minimatch.Minimatcher(...).set.Count to be 1. Actual: '$($set.Count)'"
+    }
+
+    [string[]]$literalSegments = @( )
+    [object[]]$parsedSegments = $set[0]
+    foreach ($parsedSegment in $parsedSegments) {
+        if ($parsedSegment.GetType().Name -eq 'LiteralItem') {
+            # The item is a LiteralItem when the original input for the path segment does not contain any
+            # unescaped glob characters.
+            $literalSegments += $parsedSegment.Source;
+            continue
+        }
+
+        break;
+    }
+
+    # Join the literal segments back together. Minimatch converts '\' to '/' on Windows, then squashes
+    # consequetive slashes, and finally splits on slash. This means that UNC format is lost, but can
+    # be detected from the original pattern.
+    $joinedSegments = [string]::Join('/', $literalSegments)
+    if ($joinedSegments -and ($Pattern -replace '\\', '/').StartsWith('//')) {
+        $joinedSegments = '/' + $joinedSegments # restore UNC format
+    }
+
+    # Determine the find path.
+    $findPath = ''
+    if ((Test-Rooted -Path $Pattern)) { # The pattern is rooted.
+        $findPath = $joinedSegments
+    } elseif ($joinedSegments) { # The pattern is not rooted, and literal segements were found.
+        $findPath = [System.IO.Path]::Combine($DefaultRoot, $joinedSegments)
+    } else { # The pattern is not rooted, and no literal segements were found.
+        $findPath = $DefaultRoot
+    }
+
+    # Clean up the path.
+    if ($findPath) {
+        $findPath = [System.IO.Path]::GetDirectoryName(([System.IO.Path]::Combine($findPath, '_'))) # Hack to remove unnecessary trailing slash.
+        $findPath = ConvertTo-NormalizedSeparators -Path $findPath
+    }
+
+    return New-Object psobject -Property @{
+        AdjustedPattern = Get-RootedPattern -DefaultRoot $DefaultRoot -Pattern $Pattern
+        FindPath = $findPath
+        StatOnly = $literalSegments.Count -eq $parsedSegments.Count
+    }
+}
+
+function Get-FindResult {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+        [Parameter(Mandatory = $true)]
+        $Options)
+
+    if (!(Test-Path -LiteralPath $Path)) {
+        Write-Verbose 'Path not found.'
+        return
+    }
+
+    $Path = ConvertTo-NormalizedSeparators -Path $Path
+
+    # Push the first item.
+    [System.Collections.Stack]$stack = New-Object System.Collections.Stack
+    $stack.Push((Get-Item -LiteralPath $Path))
+
+    $count = 0
+    while ($stack.Count) {
+        # Pop the next item and yield the result.
+        $item = $stack.Pop()
+        $count++
+        $item.FullName
+
+        # Traverse.
+        if (($item.Attributes -band 0x00000010) -eq 0x00000010) { # Directory
+            if (($item.Attributes -band 0x00000400) -ne 0x00000400 -or # ReparsePoint
+                $Options.FollowSymbolicLinks -or
+                ($count -eq 1 -and $Options.FollowSpecifiedSymbolicLink)) {
+
+                $childItems = @( Get-DirectoryChildItem -Path $Item.FullName -Force )
+                [System.Array]::Reverse($childItems)
+                foreach ($childItem in $childItems) {
+                    $stack.Push($childItem)
+                }
+            }
+        }
+    }
+}
+
+function Get-RootedPattern {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$DefaultRoot,
+        [Parameter(Mandatory = $true)]
+        [string]$Pattern)
+
+    if ((Test-Rooted -Path $Pattern)) {
+        return $Pattern
+    }
+
+    # Normalize root.
+    $DefaultRoot = ConvertTo-NormalizedSeparators -Path $DefaultRoot
+
+    # Escape special glob characters.
+    $DefaultRoot = $DefaultRoot -replace '(\[)(?=[^\/]+\])', '[[]' # Escape '[' when ']' follows within the path segment
+    $DefaultRoot = $DefaultRoot.Replace('?', '[?]')     # Escape '?'
+    $DefaultRoot = $DefaultRoot.Replace('*', '[*]')     # Escape '*'
+    $DefaultRoot = $DefaultRoot -replace '\+\(', '[+](' # Escape '+('
+    $DefaultRoot = $DefaultRoot -replace '@\(', '[@]('  # Escape '@('
+    $DefaultRoot = $DefaultRoot -replace '!\(', '[!]('  # Escape '!('
+
+    if ($DefaultRoot -like '[A-Z]:') { # e.g. C:
+        return "$DefaultRoot$Pattern"
+    }
+
+    # Ensure root ends with a separator.
+    if (!$DefaultRoot.EndsWith('\')) {
+        $DefaultRoot = "$DefaultRoot\"
+    }
+
+    return "$DefaultRoot$Pattern"
+}
+
+function Test-Rooted {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Path)
+
+    $Path = ConvertTo-NormalizedSeparators -Path $Path
+    return $Path.StartsWith('\') -or # e.g. \ or \hello or \\hello
+        $Path -like '[A-Z]:*'        # e.g. C: or C:\hello
+}
+
+function Trace-MatchOptions {
+    [CmdletBinding()]
+    param($Options)
+
+    Write-Verbose "MatchOptions.Dot: '$($Options.Dot)'"
+    Write-Verbose "MatchOptions.FlipNegate: '$($Options.FlipNegate)'"
+    Write-Verbose "MatchOptions.MatchBase: '$($Options.MatchBase)'"
+    Write-Verbose "MatchOptions.NoBrace: '$($Options.NoBrace)'"
+    Write-Verbose "MatchOptions.NoCase: '$($Options.NoCase)'"
+    Write-Verbose "MatchOptions.NoComment: '$($Options.NoComment)'"
+    Write-Verbose "MatchOptions.NoExt: '$($Options.NoExt)'"
+    Write-Verbose "MatchOptions.NoGlobStar: '$($Options.NoGlobStar)'"
+    Write-Verbose "MatchOptions.NoNegate: '$($Options.NoNegate)'"
+    Write-Verbose "MatchOptions.NoNull: '$($Options.NoNull)'"
+}
+
+function Trace-FindOptions {
+    [CmdletBinding()]
+    param($Options)
+
+    Write-Verbose "FindOptions.FollowSpecifiedSymbolicLink: '$($FindOptions.FollowSpecifiedSymbolicLink)'"
+    Write-Verbose "FindOptions.FollowSymbolicLinks: '$($FindOptions.FollowSymbolicLinks)'"
+}
+
 
 if ($xppDescriptorSearch.Contains("`n"))
 {
